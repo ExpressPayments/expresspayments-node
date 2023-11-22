8d3de0eb69e5c3e54ceb61106146c67affb7627c
Martin Najemi <martin@najemi.cz>
2023-11-22 23:25:06 +0000

    Update Customers.ts

--- src/resources/Customers.ts
+++ src/resources/Customers.ts
@@ -4,151 +4,151 @@ import {ExpressPlatbyResource} from '../ExpressPlatbyResource.js';
 const expressPlatbyMethod = ExpressPlatbyResource.method;
 
 export const Customers = ExpressPlatbyResource.extend({
-  create: expressPlatbyMethod({
-    method: 'POST',
-    fullPath: '/v1/customers',
-  }),
+    create: expressPlatbyMethod({
+        method: 'POST',
+        fullPath: '/v1/customers',
+    }),
 
-  retrieve: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}',
-  }),
+    retrieve: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}',
+    }),
 
-  update: expressPlatbyMethod({
-    method: 'POST',
-    fullPath: '/v1/customers/{customer}',
-  }),
+    update: expressPlatbyMethod({
+        method: 'POST',
+        fullPath: '/v1/customers/{customer}',
+    }),
 
-  list: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers',
-    methodType: 'list',
-  }),
+    list: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers',
+        methodType: 'list',
+    }),
 
-  del: expressPlatbyMethod({
-    method: 'DELETE',
-    fullPath: '/v1/customers/{customer}',
-  }),
+    del: expressPlatbyMethod({
+        method: 'DELETE',
+        fullPath: '/v1/customers/{customer}',
+    }),
 
-  createFundingInstructions: expressPlatbyMethod({
-    method: 'POST',
-    fullPath: '/v1/customers/{customer}/funding_instructions',
-  }),
+    createFundingInstructions: expressPlatbyMethod({
+        method: 'POST',
+        fullPath: '/v1/customers/{customer}/funding_instructions',
+    }),
 
-  createBalanceTransaction: expressPlatbyMethod({
-    method: 'POST',
-    fullPath: '/v1/customers/{customer}/balance_transactions',
-  }),
+    createBalanceTransaction: expressPlatbyMethod({
+        method: 'POST',
+        fullPath: '/v1/customers/{customer}/balance_transactions',
+    }),
 
-  createSource: expressPlatbyMethod({
-    method: 'POST',
-    fullPath: '/v1/customers/{customer}/sources',
-  }),
+    createSource: expressPlatbyMethod({
+        method: 'POST',
+        fullPath: '/v1/customers/{customer}/sources',
+    }),
 
-  createTaxId: expressPlatbyMethod({
-    method: 'POST',
-    fullPath: '/v1/customers/{customer}/tax_ids',
-  }),
+    createTaxId: expressPlatbyMethod({
+        method: 'POST',
+        fullPath: '/v1/customers/{customer}/tax_ids',
+    }),
 
-  deleteDiscount: expressPlatbyMethod({
-    method: 'DELETE',
-    fullPath: '/v1/customers/{customer}/discount',
-  }),
+    deleteDiscount: expressPlatbyMethod({
+        method: 'DELETE',
+        fullPath: '/v1/customers/{customer}/discount',
+    }),
 
-  deleteSource: expressPlatbyMethod({
-    method: 'DELETE',
-    fullPath: '/v1/customers/{customer}/sources/{id}',
-  }),
+    deleteSource: expressPlatbyMethod({
+        method: 'DELETE',
+        fullPath: '/v1/customers/{customer}/sources/{id}',
+    }),
 
-  deleteTaxId: expressPlatbyMethod({
-    method: 'DELETE',
-    fullPath: '/v1/customers/{customer}/tax_ids/{id}',
-  }),
+    deleteTaxId: expressPlatbyMethod({
+        method: 'DELETE',
+        fullPath: '/v1/customers/{customer}/tax_ids/{id}',
+    }),
 
-  listPaymentMethods: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}/payment_methods',
-    methodType: 'list',
-  }),
+    listPaymentMethods: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}/payment_methods',
+        methodType: 'list',
+    }),
 
-  listBalanceTransactions: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}/balance_transactions',
-    methodType: 'list',
-  }),
+    listBalanceTransactions: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}/balance_transactions',
+        methodType: 'list',
+    }),
 
-  listCashBalanceTransactions: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}/cash_balance_transactions',
-    methodType: 'list',
-  }),
+    listCashBalanceTransactions: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}/cash_balance_transactions',
+        methodType: 'list',
+    }),
 
-  listSources: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}/sources',
-    methodType: 'list',
-  }),
+    listSources: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}/sources',
+        methodType: 'list',
+    }),
 
-  listTaxIds: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}/tax_ids',
-    methodType: 'list',
-  }),
+    listTaxIds: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}/tax_ids',
+        methodType: 'list',
+    }),
 
-  retrievePaymentMethod: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}/payment_methods/{payment_method}',
-  }),
+    retrievePaymentMethod: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}/payment_methods/{payment_method}',
+    }),
 
-  retrieveBalanceTransaction: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}/balance_transactions/{transaction}',
-  }),
+    retrieveBalanceTransaction: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}/balance_transactions/{transaction}',
+    }),
 
-  retrieveCashBalance: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}/cash_balance',
-  }),
+    retrieveCashBalance: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}/cash_balance',
+    }),
 
-  retrieveCashBalanceTransaction: expressPlatbyMethod({
-    method: 'GET',
-    fullPath:
-      '/v1/customers/{customer}/cash_balance_transactions/{transaction}',
-  }),
+    retrieveCashBalanceTransaction: expressPlatbyMethod({
+        method: 'GET',
+        fullPath:
+            '/v1/customers/{customer}/cash_balance_transactions/{transaction}',
+    }),
 
-  retrieveSource: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}/sources/{id}',
-  }),
+    retrieveSource: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}/sources/{id}',
+    }),
 
-  retrieveTaxId: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/{customer}/tax_ids/{id}',
-  }),
+    retrieveTaxId: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/{customer}/tax_ids/{id}',
+    }),
 
-  search: expressPlatbyMethod({
-    method: 'GET',
-    fullPath: '/v1/customers/search',
-    methodType: 'search',
-  }),
+    search: expressPlatbyMethod({
+        method: 'GET',
+        fullPath: '/v1/customers/search',
+        methodType: 'search',
+    }),
 
-  updateBalanceTransaction: expressPlatbyMethod({
-    method: 'POST',
-    fullPath: '/v1/customers/{customer}/balance_transactions/{transaction}',
-  }),
+    updateBalanceTransaction: expressPlatbyMethod({
+        method: 'POST',
+        fullPath: '/v1/customers/{customer}/balance_transactions/{transaction}',
+    }),
 
-  updateCashBalance: expressPlatbyMethod({
-    method: 'POST',
-    fullPath: '/v1/customers/{customer}/cash_balance',
-  }),
+    updateCashBalance: expressPlatbyMethod({
+        method: 'POST',
+        fullPath: '/v1/customers/{customer}/cash_balance',
+    }),
 
-  updateSource: expressPlatbyMethod({
-    method: 'POST',
-    fullPath: '/v1/customers/{customer}/sources/{id}',
-  }),
+    updateSource: expressPlatbyMethod({
+        method: 'POST',
+        fullPath: '/v1/customers/{customer}/sources/{id}',
+    }),
 
-  verifySource: expressPlatbyMethod({
-    method: 'POST',
-    fullPath: '/v1/customers/{customer}/sources/{id}/verify',
-  }),
+    verifySource: expressPlatbyMethod({
+        method: 'POST',
+        fullPath: '/v1/customers/{customer}/sources/{id}/verify',
+    }),
 });
